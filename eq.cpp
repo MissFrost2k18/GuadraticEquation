f9124137410ee4cea7106fe892ff97e8ead3acff
MissFrost2k18 <miss.frost2017@yandex.ru>
2018-10-10 09:28:30 +0300

    Решаем квадратное уравнение. Введите коэффициенты.

--- eq.cpp
+++ eq.cpp
@@ -2,28 +2,36 @@
 #include <cmath>
 using namespace std;
 int main(){
-    double a, b, c;
-    cin >> a >> b >> c;
-    double d;
-    if (a != 0){
-        if(b == 0){
-            if(c != 0 && -c/a > 0)
-                cout << sqrt(-c/a) << " " << -sqrt(-c/a) << endl;
-            if(c == 0)
-                cout << 0 << endl;
-        }
-        else{
-            if(c==0){
-                cout << -b/a << endl;
-            }
-            else{
-                d = b*b - 4*a*c;
-                if (d > 0)
-                    cout << ((-b + sqrt(d))/(2*a)) << " " << ((-b - sqrt(d))/(2*a)) << endl;
-                if(d == 0)
-                    cout << -b/(2*a);
-            }
-        }
-    }
-    return 0;
+	double a, b, c;
+	cin >> a >> b >> c;
+	double d;
+	double x;
+	double x1, x2;
+	if(a == 0){
+		if(b == 0){
+			if(c == 0)
+				cout << "Infinity" << endl;
+			else
+				cout << "No solution" << endl;
+		}
+		else{
+			x = -c/b;
+			cout << x << endl;
+		}
+	}
+	else{
+		d = b*b - 4*a*c;
+		if(d < 0)
+			cout << "No solution" << endl;
+		if(d == 0){
+			x = -b/(2*a);
+			cout << x << endl;
+		}
+		if(d > 0){
+			x1 = (-b + sqrt(d))/(2*a);
+			x2 = (-b - sqrt(d))/(2*a);
+			cout << x1 << " " << x2 << endl;
+		}
+	}
+	return 0;
 }
